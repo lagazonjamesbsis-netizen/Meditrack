d677078685877da048773ee2283a69a68829562d
techdev <techdev@xdnainteractive.com>
2026-08-15 15:22:19 +0800

    Rename NextCrud branding to MediTrack

--- config/constants.ts
+++ config/constants.ts
@@ -1,12 +1,12 @@
-export const APP_NAME = 'NextCrud'
+export const APP_NAME = 'MediTrack'
 export const APP_BASE_URL =
   process.env.NODE_ENV === 'production'
-    ? 'https://nextcrud.vercel.app'
+    ? 'https://meditrack.vercel.app'
     : 'http://localhost:3000'
 
-export const SCHOOL_NAME = 'NextCrud'
+export const SCHOOL_NAME = 'MediTrack'
 
-export const SMTP_FROM_NAME = 'NextCrud'
+export const SMTP_FROM_NAME = 'MediTrack'
 export const SMTP_FROM_EMAIL = 'hello@domain.com'
 
 export const USERS_PER_PAGE = 5
